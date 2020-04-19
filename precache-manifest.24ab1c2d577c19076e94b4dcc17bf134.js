a32906fc265a5c1e37df66e89c7f64b34d45c045
PierrickBlons <PierrickBlons@users.noreply.github.com>
2020-04-19 21:39:14 +0000

    deploy: 73dbb182f1411839b370f69aa4d890c965953691

---
+++ precache-manifest.24ab1c2d577c19076e94b4dcc17bf134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4153f546b74d3db20cf6ebdf33215f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19fa81fe470f5c5bceb1",
+    "url": "/static/js/2.020cab5f.chunk.js"
+  },
+  {
+    "revision": "d314c77d1d45d960a8ca1925ff4b1296",
+    "url": "/static/js/2.020cab5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31fc51afb1e94bdfe99",
+    "url": "/static/js/main.52afda78.chunk.js"
+  },
+  {
+    "revision": "cd5cc0b019c8434d78aa",
+    "url": "/static/js/runtime-main.95a77404.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f1ba6d016f5dcce91a5aceba3403280.js"
+  "/precache-manifest.24ab1c2d577c19076e94b4dcc17bf134.js"
 );
 
 self.addEventListener('message', (event) => {
